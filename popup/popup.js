ee6ac9ca4ac76dc88cb7a5d9717ee6ab46793efa
Charles <knappcharles@icloud.com>
2023-09-01 20:09:35 -0400

    Minor changes to fix grouping initialization. Fixed addition of API Key.

--- popup/popup.js
+++ popup/popup.js
@@ -2,26 +2,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const apiKeyInput = document.getElementById("apiKey");
     const saveApiKeyButton = document.getElementById("saveApiKeyButton");
     const toggleExtension = document.getElementById("toggleExtension");
+    const apiKeyEntrySlide = document.getElementById("apiKeyEntry");
     const settingsSlide = document.getElementById("settings");
+    const welcomeSlide = document.getElementById("welcomeSlide");
   
-    // Load extension settings from storage
+    function showSlide(slide) {
+      apiKeyEntrySlide.style.display = "none";
+      settingsSlide.style.display = "none";
+      welcomeSlide.style.display = "none";
+  
+      slide.style.display = "block";
+    }
+  
     chrome.storage.local.get(['apiKey', 'enabled'], function (data) {
       const apiKey = data.apiKey;
       const isEnabled = data.enabled;
   
       if (apiKey) {
+        showSlide(settingsSlide);
         apiKeyInput.value = apiKey;
-        settingsSlide.style.display = "block"; // Show the settings slide
         toggleExtension.checked = isEnabled;
+      } else {
+        showSlide(apiKeyEntrySlide);
       }
     });
   
     saveApiKeyButton.addEventListener("click", function () {
       const apiKey = apiKeyInput.value.trim();
       if (apiKey) {
-        // Save the API key in extension storage
         chrome.storage.local.set({ apiKey: apiKey }, function () {
-          settingsSlide.style.display = "block"; // Show the settings slide
+          showSlide(settingsSlide);
         });
       }
     });
@@ -30,4 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const isEnabled = toggleExtension.checked;
       chrome.storage.local.set({ enabled: isEnabled });
     });
-  });  
+  
+    showSlide(welcomeSlide);
+  });
+  
